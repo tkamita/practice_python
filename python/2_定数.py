631145f9feb1b577048c53c113b30cf517193920
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-10-15 18:36:05 +0900

    定数

---
+++ python/2_定数.py
@@ -0,0 +1,19 @@
+# 定数
+#   全て大文字で定義する
+LEGAL_AGE = 20
+age = 18
+if age < LEGAL_AGE:
+  print('未成年')
+else:
+  print('成人')
+    
+
+# 予約後
+#   変数、定数で使わない
+#   ３３個ある
+#     for
+#     try
+
+# 特殊なprint文
+print(f'age = {age}') #3.6
+print(f'{age=}') #3.8
