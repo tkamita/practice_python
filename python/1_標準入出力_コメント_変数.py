a6708513dd9d747c66f6c3459a6b71f83ae57005
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-10-15 18:22:27 +0900

    python

---
+++ python/1_標準入出力_コメント_変数.py
@@ -0,0 +1,21 @@
+# 標準入出力_コメント_変数
+
+# 出力
+print("hello world")
+
+# 入力
+# input
+name = input('あなたの名前は何ですか?')
+print(name)
+
+
+# 変数の埋め込み
+# format関数
+print('私の名前は = {}'.format(name))
+
+age = 18
+print('私の名前は = {}, 年齢は{}'.format(name, age))
+
+# 二つの変数に代入
+a = b = 'hello'
+print(a, b)
