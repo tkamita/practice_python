8ad63bcb58c1ff9b8b2b7f7f173895d7fb08c93a
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-10-15 22:33:37 +0900

    論理型

---
+++ python/3_論理型_and_or.py
@@ -0,0 +1,22 @@
+# 論理型
+#   true  false の２つを扱う
+is_animal = False
+if is_animal:
+  print('動物です')
+
+
+
+
+is_adult = False
+is_man = False
+# or
+#   論理和
+if is_man or is_adult:
+  print('男か大人です')
+
+# and
+#   論理積
+if is_man and is_adult:
+  print('成人男性です')
+
+
