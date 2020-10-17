8b53ed294c0482aab01141227395c7aad37d22ab
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-10-17 15:22:00 +0900

    数値、文字列・変換

---
+++ python/6_数値_文字列の変換.py
@@ -0,0 +1,25 @@
+# 数値型から文字列への変換
+#   str関数
+
+# 文字列型から数値型への変換
+#   int関数
+#   float関数
+
+#int str 変換
+int_num = 12
+str_num = str(int_num)
+print(str_num)
+print(type(str_num))
+print('num = ' + str(int_num))
+float_num = -20.5
+str_float = str(float_num)
+print(str_float)
+print(type(str_float))
+
+
+#str int, float
+msg = '12'
+int_msg = int(msg)
+float_msg = float(msg)
+print('value = {}, type = {}'.format(int_msg, type(int_msg)))
+print('value = {}, type = {}'.format(float_msg, type(float_msg)))
