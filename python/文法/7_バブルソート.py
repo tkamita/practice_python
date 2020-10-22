0b98dc1e9e3976a882faee2381d4381da4f2a881
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-10-22 16:47:06 +0900

    バブルソート

--- python/文法/7_バブルソート.py
+++ python/文法/7_バブルソート.py
@@ -8,7 +8,19 @@ for i in range(len(list_a)):
   #一回のソートで配列の何番目までいくか
   # 一回目は右端、ソートする範囲を狭める
   for j in range(0, len(list_a) - i - 1):
-    if list_a[j] > list_a[j+1]:
+    if list_a[j] > list_a[j+1]:   #昇順
+      list_a[j], list_a[j+1] = list_a[j+1], list_a[j]
+
+print(list_a)
+
+
+# i:   0 => list_aの長さ-1
+for i in range(len(list_a)):
+  # j:  0 => list_aの長さ-1 -i -1
+  #一回のソートで配列の何番目までいくか
+  # 一回目は右端、ソートする範囲を狭める
+  for j in range(0, len(list_a) - i - 1):
+    if list_a[j] < list_a[j+1]:   #降順
       list_a[j], list_a[j+1] = list_a[j+1], list_a[j]
 
 print(list_a)
