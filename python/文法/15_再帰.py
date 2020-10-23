67ed34edfa7db529dba102300e53edfe8b725db4
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-10-23 14:18:52 +0900

    再帰

---
+++ python/文法/15_再帰.py
@@ -0,0 +1,22 @@
+# 関数から自分の関数を呼び出す
+
+
+def sample(a):
+  if a < 0:
+    return
+  else:
+    print(a)
+    sample(a-1)
+
+sample(10)
+
+
+
+# フィボナッチ
+def fib(n):
+  return 1 if n < 3 else fib(n-1) + fib(n-2)
+
+
+for x in range(1, 10):
+  print(fib(x))
+  
