774f62684ea0a4a16fcd17ef48dea015ee46d298
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-10-23 15:00:44 +0900

    デコレータ関数

---
+++ python/文法/17_デコレータ関数.py
@@ -0,0 +1,21 @@
+
+
+
+def my_decorator(func):
+  def wrapper(*args, **kwargs):
+    print('*' * 100)
+    func(*args, **kwargs)
+    print('*' * 100)
+  return wrapper
+
+@my_decorator
+def func_a(*args, **kwargs):
+  print('func_aを実行')
+  print(args)
+@my_decorator
+def func_b(*args, **kwargs):
+  print('func_bを実行')
+  print(args)
+
+func_a(1, 2, 3)
+func_b(1, 2, 3)
