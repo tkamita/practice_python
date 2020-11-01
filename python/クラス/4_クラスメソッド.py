471a7ef455fcc830596c8662c4a68ef41c0c9fab
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-11-01 16:08:58 +0900

    クラスメソッド

---
+++ python/クラス/4_クラスメソッド.py
@@ -0,0 +1,42 @@
+# インスタンスメソッド
+  # 引数のselfは必ずつける
+  # selfは自分自身（作成したインスタンス）を表す
+# クラスメソッド
+  # @classmethodで定義
+  # 第一引数をcls
+# スタティックメソッド
+  # @staticmethod
+  # クラス変数へもインスタンスへ変数へもアクセスできない
+
+
+
+class Human:
+  class_name = "Human"    #クラス変数
+  def __init__(self, name, age):   #コンストラクタ
+    self.name = name
+    self.age = age
+
+  def print_name_age(self):   #インスタンスメソッド
+    print('インスタンスメソッド実行')
+    print('name = {}, age = {}'.format(self.name, self,age))
+
+    @classmethod
+    def print_class_name(cls, msg):   #クラスメソッド
+      print('クラスメソッド実行')
+      print(cls.class_name)   #クラス変数
+      print(msg)
+
+    @staticmethod
+    def print_msg(msg):   #スタティックメソッド
+      print('スタティックメソッド実行')
+      print(msg)
+
+
+
+Human.print_class_name('こんにちは')
+man = Human('桜木', 18)
+man.print_name_age()
+man.print_msg('hello')
+Human.print_msg('hello static')
+
+
